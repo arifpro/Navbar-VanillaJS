941fc684f2250861f4b26afaa450024debb426be
arifpro <arif.swfu@outlook.com>
2020-06-13 10:11:45 +0800

    done

--- app.js
+++ app.js
@@ -3,3 +3,21 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+
+
+// toggleBar icon button
+const navToggle = document.querySelector('.nav-toggle')
+// nav items
+const links = document.querySelector('.links')
+
+
+navToggle.addEventListener('click', function() {
+    // if (links.classList.contains('show-links')) {
+    //     links.classList.remove('show-links')
+    // } else {
+    //     links.classList.add('show-links')
+    // }
+
+    links.classList.toggle('show-links')
+})
